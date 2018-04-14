fde62ea2f629c090f43bbc6b0aa7439e6e7893d4
Michael DiCicco <mddicicco@ualr.edu>
2018-04-14 18:10:17 -0500

    Fixed?

--- scripts/twit.py
+++ scripts/twit.py
@@ -1,17 +1,38 @@
 import tweepy
 import json
 
-credentials = None
 
-with open("creds.json", "r") as f:
-    json.loads(f)
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def get_tweets(creds, username):
+    # Authorization to consumer key and consumer secret
+    auth = tweepy.OAuthHandler(creds["consumer_key"], creds["consumer_secret"])
 
-api = tweepy.API(auth)
+    # Access to user's access key and access secret
+    auth.set_access_token(creds["access_token"], creds["access_token_secret"])
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+    # Calling API
+    api = tweepy.API(auth)
 
-print('Hello World')
+    # 200 tweets to be extracted
+    number_of_tweets=200
+    tweets = api.user_timeline(screen_name=username)
+
+    # empty list
+    tmp = []
+    for tweet in tweets:
+        tmp.append(tweet.text)
+    print(tmp)
+
+
+def main():
+
+    twitacct = "realDonaldTrump"
+    credentials = None
+    with open("creds.json", "r") as f:
+        credentials = json.load(f)
+
+    get_tweets(credentials, twitacct)
+
+
+if __name__ == "__main__":
+    main()
+
